c88a09a7c681de1f0e7f4b43cd34b518b901432f
Walker <walker@praiseweb.com.br>
2019-04-06 18:54:31 -0300

    remove menus

--- packages/shop-store/shop-store.php
+++ packages/shop-store/shop-store.php
@@ -16,3 +16,20 @@ function shop_register_product() {
 }
 
 add_action('init', 'shop_register_product');
+
+// Remove Posts and Pages
+function shop_remove_menus() {
+  global $menu;
+  $restricted = array(
+    __(__('Posts')),
+    __(__('Pages')),
+    __(__('Comments')),
+    __(__('Appearance')),
+  );
+  end ($menu);
+  while (prev($menu)){
+    $value = explode(' ',$menu[key($menu)][0]);
+    if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+  }
+}
+add_action('admin_menu', 'shop_remove_menus');
