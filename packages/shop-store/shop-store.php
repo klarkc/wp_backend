b352767961a898f7d08bf5f941be349a3bcc5e79
Walker <walker@praiseweb.com.br>
2019-04-06 18:50:27 -0300

    add shop_product

--- packages/shop-store/shop-store.php
+++ packages/shop-store/shop-store.php
@@ -2,3 +2,17 @@
 /**
  *  Plugin Name: Shop Store
  * */
+
+function shop_register_product() {
+  // Register shop_product
+  register_post_type( 'shop_product',
+    array(
+      'label' => 'Produto',
+      'public' => true,
+      'has_archive' => true,
+      'show_in_rest' => true,
+    )
+  );
+}
+
+add_action('init', 'shop_register_product');
